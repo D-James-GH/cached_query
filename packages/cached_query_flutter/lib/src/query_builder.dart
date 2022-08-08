786d4bf295e81704c6b490b86c7be5400bc66b44
Daniel James <221g.nad@gmail.com>
2022-08-08 20:22:10 +0100

    feat(cached_query_flutter): add ability to use query key only

--- packages/cached_query_flutter/lib/src/query_builder.dart
+++ packages/cached_query_flutter/lib/src/query_builder.dart
@@ -8,36 +8,68 @@ import 'package:flutter/material.dart';
 /// {@endtemplate}
 class QueryBuilder<T> extends StatefulWidget {
   /// The [Query] to used to update the ui.
-  final Query<T> query;
+  final Query<T>? query;
 
   /// Called on each widget build.
   ///
   /// Passes [BuildContext], [QueryState].
   final Widget Function(BuildContext context, QueryState<T> state) builder;
+  final Object? queryKey;
 
   /// {@macro queryBuilder}
   const QueryBuilder({
     Key? key,
-    required this.query,
+    this.query,
+    this.queryKey,
     required this.builder,
-  }) : super(key: key);
+  })  : assert(
+          query != null || queryKey != null,
+          "Query key or a query must be provided.",
+        ),
+        super(key: key);
 
   @override
   State<QueryBuilder<T>> createState() => _QueryBuilderState<T>();
 }
 
 class _QueryBuilderState<T> extends State<QueryBuilder<T>> {
+  late Query<T> _query;
   late QueryState<T> _state;
+
   StreamSubscription<QueryState<T>>? _subscription;
+
   @override
   void initState() {
     super.initState();
-    _state = widget.query.state;
-    _subscription = widget.query.stream.listen((state) {
-      setState(() {
-        _state = state;
-      });
-    });
+    if (widget.queryKey != null) {
+      final q = CachedQuery.instance.getQuery(widget.queryKey!);
+      assert(q != null, "No query found with the key ${widget.queryKey}");
+      assert(q is Query<T>, "Query found is not of type $T");
+      _query = q! as Query<T>;
+    }
+    if (widget.query != null) {
+      _query = widget.query!;
+    }
+    _subscribe();
+    _state = _query.state;
+  }
+
+  @override
+  void didUpdateWidget(covariant QueryBuilder<T> oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    final oldQuery =
+        oldWidget.query ?? CachedQuery.instance.getQuery(oldWidget.queryKey!);
+    final currentQuery =
+        widget.query ?? CachedQuery.instance.getQuery(widget.queryKey!);
+    assert(currentQuery is Query<T>, "Query found is not of type $T");
+    if (oldQuery != currentQuery) {
+      if (_subscription != null) {
+        _unsubscribe();
+        _query = currentQuery as Query<T>;
+        _state = _query.state;
+      }
+      _subscribe();
+    }
   }
 
   @override
@@ -47,8 +79,21 @@ class _QueryBuilderState<T> extends State<QueryBuilder<T>> {
 
   @override
   void dispose() {
+    _unsubscribe();
+    super.dispose();
+  }
+
+  void _subscribe() {
+    _state = _query.state;
+    _subscription = _query.stream.listen((state) {
+      setState(() {
+        _state = state;
+      });
+    });
+  }
+
+  void _unsubscribe() {
     _subscription?.cancel();
     _subscription = null;
-    super.dispose();
   }
 }
