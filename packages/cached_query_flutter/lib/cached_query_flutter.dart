2e94ab5763ca8da562535b031558fe76de7cc149
Erwin Knoop <erwinknupke@gmail.com>
2024-05-27 12:38:13 +0200

    Added query listener and consumer widgets

--- packages/cached_query_flutter/lib/cached_query_flutter.dart
+++ packages/cached_query_flutter/lib/cached_query_flutter.dart
@@ -6,4 +6,8 @@ export 'src/cached_query_flutter.dart';
 export 'src/infinite_query_builder.dart';
 export 'src/query_config_flutter.dart';
 export 'src/mutation_builder.dart';
+export 'src/mutation_consumer.dart';
+export 'src/mutation_listener.dart';
 export 'src/query_builder.dart';
+export 'src/query_consumer.dart';
+export 'src/query_listener.dart';
