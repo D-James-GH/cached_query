9e1a88dd5262fa5d43ef4b4cc32c8cb7618c5579
Dan G <55734821+D-James-GH@users.noreply.github.com>
2024-09-14 16:20:13 -0700

    chore: missed files

---
+++ examples/multiple_caches/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:cached_query_flutter/cached_query_flutter.dart';
+import 'package:flutter/material.dart';
+import 'package:multiple_caches/post/post_page.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Set the default configuration for the CachedQuery instance.
+  CachedQuery.instance.configFlutter(
+    config: QueryConfigFlutter(
+      refetchDuration: const Duration(seconds: 4),
+    ),
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Demo',
+      home: PostPage(),
+    );
+  }
+}
