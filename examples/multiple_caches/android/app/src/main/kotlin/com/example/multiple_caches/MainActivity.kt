42290f4a1d06b7532a2e8003456c48c7a8b0e634
Dan G <55734821+D-James-GH@users.noreply.github.com>
2024-09-14 16:20:13 -0700

    chore: missed files

---
+++ examples/multiple_caches/android/app/src/main/kotlin/com/example/multiple_caches/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.multiple_caches
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ examples/multiple_caches/lib/post/post_model/post_model.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'post_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$PostModelImpl _$$PostModelImplFromJson(Map<String, dynamic> json) =>
+    _$PostModelImpl(
+      id: (json['id'] as num).toInt(),
+      title: json['title'] as String,
+      body: json['body'] as String,
+      userId: (json['userId'] as num).toInt(),
+    );
+
+Map<String, dynamic> _$$PostModelImplToJson(_$PostModelImpl instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'title': instance.title,
+      'body': instance.body,
+      'userId': instance.userId,
+    };
